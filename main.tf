f302fc3e8d318b6023498290e339e1bd03db42cd
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-05-22 16:17:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 provider "aws" {
-    region = "us-west-2"
     profile = "default"
+    region = "us-west-2"
+    
 }
 
+ resource "aws_vpc" "venkat_vpc"{
+    cidr_block = "190.160.0.0/16"
+    instance_tenancy = "default"
+}
 
- 
-resource "aws_instance" "jenkins"{
+resource "aws_instance" "git-jenkins"{
     ami     = "ami-0cf6f5c8a62fa5da6"
     tenancy = "default"
     instance_type = "t2.micro"

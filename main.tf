6dde49c09bd33236504e606284eac7af6b93e2c0
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-05-25 11:59:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,15 @@ provider "aws" {
     
 }
 
-resource "aws_instance" "git-jenkins"{
-    ami     = "ami-0cf6f5c8a62fa5da6"
-    tenancy = "default"
-    instance_type = "t2.micro"
+
+
+resource "aws_vpc" "venkat_vpc"{
+    cidr_block = "190.160.0.0/16"
+    instance_tenancy = "default"
+
+    tags = {
+        Name = "venkat_vpc"
+        Location = "bangalore"
+
+    }
 }
